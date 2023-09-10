ccc6ecbe6d94b29506959e57bad5ef6739319ff7
Hanad <hanadmse@gmail.com>
2023-09-10 13:27:25 -0500

    First commit
    First commit

---
+++ src/main/java/dev/hanad/movies/MovieController.java
@@ -0,0 +1,33 @@
+package dev.hanad.movies;
+
+import org.apache.coyote.Response;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/v1/movies")
+@CrossOrigin
+public class MovieController {
+    @Autowired
+    private MovieService movieService;
+
+    @GetMapping
+    public ResponseEntity<List<Movie>> getAllMovies() {
+            return new ResponseEntity<List<Movie>>(movieService.allMovies(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{imdbId}")
+    public ResponseEntity<Optional<Movie>> getSingleMovie(@PathVariable String imdbId) {
+        return new ResponseEntity<Optional<Movie>>(movieService.singleMovie(imdbId), HttpStatus.OK);
+    }
+}
+
